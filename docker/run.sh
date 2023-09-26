bd242622c6582624f65dbbb28add5374e681ea31
a1ex <12754559+a1ex4@users.noreply.github.com>
2023-09-26 17:32:26 +0000

    add docker image

---
+++ docker/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Retrieve from Environment variables, or use 1000 as default
+gid=${PGID:-1000}
+uid=${PUID:-1000}
+
+! getent group "${gid}" && addgroup -g "${gid}" -S ownfoil
+GROUP=$(getent group "${gid}" | cut -d ":" -f 1)
+! getent passwd "${uid}" && adduser -u "${uid}" -G "${GROUP}" -S ownfoil
+
+chown -R ${uid}:${gid} /app
+
+echo "Starting ownfoil"
+
+exec sudo -E -u "#${uid}" python /app/app.py
