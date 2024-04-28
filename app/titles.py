0353b2593c65cddc8e3ad44c3187bbf148a45af5
Alexandre Pulido <apulido@free.fr>
2024-04-28 02:26:32 +0200

    don't download and build titledb locally, use regional titles.json build by GA

--- app/titles.py
+++ app/titles.py
@@ -3,6 +3,8 @@ import sys
 import re
 import json
 import hashlib
+import unzip_http
+import requests
 from constants import *
 from pathlib import Path
 from binascii import hexlify as hx, unhexlify as uhx
@@ -103,7 +105,7 @@ def load_titledb(app_settings):
     with open(os.path.join(TITLEDB_DIR, 'cnmts.json')) as f:
         cnmts_db = json.load(f)
 
-    with open(os.path.join(TITLEDB_DIR, f"{app_settings['library']['region']}.{app_settings['library']['language']}.json")) as f:
+    with open(os.path.join(TITLEDB_DIR, get_region_titles_file(app_settings))) as f:
         titles_db = json.load(f)
 
     with open(os.path.join(TITLEDB_DIR, 'versions.json')) as f:
@@ -255,28 +257,69 @@ def get_all_existing_dlc(title_id):
                     dlcs.append(app_id.upper())
     return dlcs
 
-def set_titledb_default_files():
-    os.system(f"cd {TITLEDB_DIR} && git sparse-checkout set {'/' + ' /'.join(TITLEDB_DEFAULT_FILES)} --no-cone")
+def get_region_titles_file(app_settings):
+    return f"titles.{app_settings['library']['region']}.{app_settings['library']['language']}.json"
 
-def set_titledb_lang_file(region, language):
-    os.system(f'cd {TITLEDB_DIR} && git sparse-checkout add /{region}.{language}.json')
+def download_from_remote_zip(rzf, path, store_path):
+    with rzf.open(path) as fpin:
+        with open(store_path, mode='wb') as fpout:
+            while True:
+                r = fpin.read(65536)
+                if not r:
+                    break
+                fpout.write(r)
 
-def git_fetch_and_pull():
-    os.system(f'cd {TITLEDB_DIR} && git checkout master')
-    os.system(f'cd {TITLEDB_DIR} && git fetch && git pull')
+def is_titledb_update_available(rzf):
+    update_available = False
+    local_commit_file = os.path.join(TITLEDB_DIR, '.latest')
+    remote_latest_commit_file = [ f.filename for f in rzf.infolist() if 'latest_' in f.filename ][0]
+    latest_remote_commit = remote_latest_commit_file.split('_')[-1]
+
+    if not os.path.isfile(local_commit_file):
+        print('Retrieving titledb for the first time...')
+        update_available = True
+    else: 
+        with open(local_commit_file, 'r') as f:
+            current_commit = f.read()
+            
+        if current_commit == latest_remote_commit:
+            print(f'Titledb already up to date, commit: {current_commit}')
+            update_available = False
+        else:
+            print(f'Titledb update available, current commit: {current_commit}, latest commit: {latest_remote_commit}')
+            update_available = True
+    
+    if update_available:
+        with open(local_commit_file, 'w') as f:
+            f.write(latest_remote_commit)
+    
+    return update_available
+
+def download_titledb_files(rzf, files):
+    for file in files:
+        store_path = os.path.join(TITLEDB_DIR, file)
+        print(f'Downloading {file} from remote titledb to {store_path}')
+        download_from_remote_zip(rzf, file, store_path)
+
 
 def update_titledb_files(app_settings):
-    os.system(f"cd {TITLEDB_DIR} && git reset --hard")
-    set_titledb_default_files()
-    set_titledb_lang_file(app_settings['library']['region'], app_settings['library']['language'])
-    
+    files_to_update = TITLEDB_DEFAULT_FILES
+    region_titles_file = get_region_titles_file(app_settings)
+    files_to_update.append(region_titles_file)
+
+    region_titles_file_present = region_titles_file in os.listdir(TITLEDB_DIR)
+
+    r = requests.get(TITLEDB_ARTEFACTS_URL, allow_redirects = False)
+    direct_url = r.next.url
+    rzf = unzip_http.RemoteZipFile(direct_url)
+    if is_titledb_update_available(rzf) or not region_titles_file_present:
+        download_titledb_files(rzf, files_to_update)
+
 def update_titledb(app_settings):
     print('Updating titledb...')
     if not os.path.isdir(TITLEDB_DIR):
-        print('Retrieving titledb for the first time...')
-        os.system(f'git clone --depth=1 --no-checkout {TITLEDB_URL} {TITLEDB_DIR}')
+        os.makedirs(TITLEDB_DIR, exist_ok=True)
 
     update_titledb_files(app_settings)
-    git_fetch_and_pull()
     load_titledb(app_settings)
     print('titledb update done.')
