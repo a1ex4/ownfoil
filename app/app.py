0b769cf44e26360b035943c3d83f87584a174a63
Alexandre Pulido <apulido@free.fr>
2025-08-26 12:50:04 +0200

    fix(library): fix library not showing in the UI if cached file is corruped:  - prevent concurrent writes to library file to avoid corruption  - regenerate library file if not valid

--- app/app.py
+++ app/app.py
@@ -450,7 +450,7 @@ def serve_game(id):
 @debounce(10)
 def post_library_change():
     with app.app_context():
-        load_titledb(app_settings)
+        titles.load_titledb()
         process_library_identification(app)
         add_missing_apps_to_db()
         update_titles() # Ensure titles are updated after identification
@@ -460,7 +460,7 @@ def post_library_change():
         # So, we just need to ensure titles_library is updated from the generated library
         generate_library()
         titles.identification_in_progress_count -= 1
-        unload_titledb()
+        titles.unload_titledb()
 
 @app.post('/api/library/scan')
 @access_required('admin')
